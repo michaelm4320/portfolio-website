0ae6a6dc050365a64abf25d49228c0e0977f15aa
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-11 21:06:10 -0400

    fix spring boot and disable database temporarily, move project structure

--- src/main/java/com/example/portfolio_backend/PortfolioBackendApplication.java
+++ src/main/java/com/example/portfolio_backend/PortfolioBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
