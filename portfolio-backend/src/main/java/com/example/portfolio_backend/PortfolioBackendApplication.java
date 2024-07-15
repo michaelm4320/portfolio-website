886692211faf345ab94b720192941a9bd9f5d9c5
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-15 17:46:01 -0400

    fix spring boot and database integration

--- portfolio-backend/src/main/java/com/example/portfolio_backend/PortfolioBackendApplication.java
+++ portfolio-backend/src/main/java/com/example/portfolio_backend/PortfolioBackendApplication.java
@@ -3,12 +3,13 @@ package com.example.portfolio_backend;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.sql.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
