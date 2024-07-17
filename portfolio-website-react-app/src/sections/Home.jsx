037f56ea20a5256306a71b3bc81ce99c56fe28dc
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-16 20:49:00 -0400

    add sections directory with Home.jsx

---
+++ portfolio-website-react-app/src/sections/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import testImg from '../assets/testImg.png';
+
+export default function Home() {
+  return (
+    <section id="homeSection" className="home--section">
+      <div className="home--section--content--box">
+        <div className="home--section--content">
+          <p className="section--title">Hey, I'm John</p>
+          <h1 className="home--section--title">
+            <span className="home--section-title--color">Software</span>{" "}
+            <br />
+            Engineer
+          </h1>
+          <p className="home--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Contact Me</button>
+      </div>
+<div className="home--section--img">
+        <img src={testImg} alt="home Section" />
+      </div>
+    </section>
+  );
+}
