55383dff6ba2201f579aa2a6884e36acc6006c98
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-23 20:11:00 -0400

    create home section

--- portfolio-website-react-app/src/sections/Home.jsx
+++ portfolio-website-react-app/src/sections/Home.jsx
@@ -4,22 +4,15 @@ import testImg from '../assets/testImg.png';
 export default function Home() {
   return (
     <section id="homeSection" className="home--section">
-      <div className="home--section--content--box">
-        <div className="home--section--content">
-          <p className="section--title">Hey, I'm John</p>
-          <h1 className="home--section--title">
-            <span className="home--section-title--color">Full Stack</span>{" "}
-            <br />
-            Developer
-          </h1>
-          <p className="home--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
-          </p>
+      <div className="home-section-content">
+        <div className="home-section-text">
+            <p>Hello, I'm Michael</p>
+            <h1>Software Engineer</h1>
+            <p>Passionate about developing innovative software solutions to solve real-world problems.</p>
+            <button className="btn-contact">Contact Me</button>
         </div>
-        <button className="btn btn-primary">Contact Me</button>
       </div>
-<div className="home--section--img">
+      <div className="home-section-img">
         <img src={testImg} alt="home Section" />
       </div>
     </section>
