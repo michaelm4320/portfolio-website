bf0111ac9feaf34dd73856f674e8edd6d52122ce
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-31 22:25:48 -0400

    organize home.jsx code

--- portfolio-website-react-app/src/sections/Home.jsx
+++ portfolio-website-react-app/src/sections/Home.jsx
@@ -5,27 +5,27 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 export default function Home() {
   return (
-        <div className="parallax-container">
-          <Parallax pages={2}>
+    <div className="parallax-container">
+        <Parallax pages={2}>
             <ParallaxLayer offset={0} speed={0}>
-    <section id="homeSection" className="home-section">
-      <div className="home-section-content">
-        <div className="home-section-text">
-            <p className="home-section-greeting">Hello, I'm Michael</p>
-            <h1 className="home-section-title">Software
-            <br />
-            Engineer
-            </h1>
-            <p className="home-section-description">Passionate about developing innovative software solutions to solve real-world problems.</p>
-            <button className="btn-contact">Contact Me</button>
-        </div>
-      </div>
-      <div className="home-section-img">
-        <img src={testImg} alt="home Section" />
-      </div>
-    </section>
-     </ParallaxLayer>
-            </Parallax>
-          </div>
+                <section id="homeSection" className="home-section">
+                  <div className="home-section-content">
+                    <div className="home-section-text">
+                        <p className="home-section-greeting">Hello, I'm Michael</p>
+                        <h1 className="home-section-title">Software
+                        <br />
+                        Engineer
+                        </h1>
+                        <p className="home-section-description">Passionate about developing innovative software solutions to solve real-world problems.</p>
+                        <button className="btn-contact">Contact Me</button>
+                    </div>
+                  </div>
+                  <div className="home-section-img">
+                    <img src={testImg} alt="home Section" />
+                  </div>
+                </section>
+            </ParallaxLayer>
+        </Parallax>
+    </div>
   );
 }
