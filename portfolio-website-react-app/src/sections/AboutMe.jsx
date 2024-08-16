52bbf12ced20b17883407a248a8177c011c3afd0
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-08-16 18:20:16 -0400

    Reformat files

--- portfolio-website-react-app/src/sections/AboutMe.jsx
+++ portfolio-website-react-app/src/sections/AboutMe.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import testImg from '../assets/testImgF1.png';
 import circles from '../assets/circles.png';
-import Computer from '../canvas/Computers.jsx'; // Adjust the path as necessary
+import Computer from '../canvas/Computers.jsx';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 export default function AboutMe() {
   return (
-                <section id="aboutSection" className="about-section">
-                  <div className="about-section-content">
-                    <div className="about-section-img">
-                        <img src={testImg} alt="about Section" />
-                    </div>
-                    <div className="about-section-text">
-                        <h1 className="about-section-title">About Me
-                        </h1>
-                        <p className="about-section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                  </div>
-                </section>
+      <section id="aboutSection" className="about-section">
+        <div className="about-section-content">
+            <div className="about-section-img">
+                <img src={testImg} alt="about Section" />
+            </div>
+            <div className="about-section-text">
+                <h1 className="about-section-title">About Me</h1>
+                <p className="about-section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+      </section>
   );
 }
--- portfolio-website-react-app/src/sections/Home.jsx
+++ portfolio-website-react-app/src/sections/Home.jsx
@@ -7,21 +7,21 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 export default function Home() {
   return (
-                <section id="homeSection" className="home-section">
-                  <div className="home-section-content">
-                    <div className="home-section-text">
-                        <p className="home-section-greeting">Hello, I'm Michael</p>
-                        <h1 className="home-section-title">Software
-                        <br />
-                        Engineer
-                        </h1>
-                        <p className="home-section-description">Passionate about developing innovative software solutions to solve real-world problems.</p>
-                        <button className="btn-contact">Contact Me</button>
-                    </div>
-                  </div>
-                  <div className="home-section-model">
-                    <Computer />
-                  </div>
-                </section>
+    <section id="homeSection" className="home-section">
+        <div className="home-section-content">
+            <div className="home-section-text">
+                <p className="home-section-greeting">Hello, I'm Michael</p>
+                <h1 className="home-section-title">Software
+                <br />
+                Engineer
+                </h1>
+                <p className="home-section-description">Passionate about developing innovative software solutions to solve real-world problems.</p>
+                <button className="btn-contact">Contact Me</button>
+            </div>
+        </div>
+        <div className="home-section-model">
+            <Computer />
+        </div>
+    </section>
   );
 }
