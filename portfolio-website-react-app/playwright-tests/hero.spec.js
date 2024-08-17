af5dc49fdbfc0ea80c8cba933cdd01912df8a364
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-08-17 18:32:31 -0400

    fix playwright test

--- portfolio-website-react-app/playwright-tests/hero.spec.js
+++ portfolio-website-react-app/playwright-tests/hero.spec.js
@@ -4,5 +4,5 @@ const { test, expect } = require('@playwright/test');
 test('hero section has greeting text', async ({ page }) => {
   await page.goto('/'); // Base URL from playwright.config.cjs
   const greeting = await page.textContent('.hero-section-greeting');
-  expect(greeting).toBe("Hello, I'm Michael");
+  expect(greeting).toBe("Hello, I'm Michael!");
 });
