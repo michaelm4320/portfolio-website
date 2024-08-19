ebabab488049b337301aeffd5df6c3d49608ba4a
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-08-18 22:21:25 -0400

    add new test image, update css with hologram for about me

--- portfolio-website-react-app/src/sections/AboutMe.jsx
+++ portfolio-website-react-app/src/sections/AboutMe.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import testImg from '../assets/testImgF1.png';
-import circles from '../assets/circles.png';
-import Computer from '../canvas/Computers.jsx';
+import testImg from '../assets/testImgcircle.png';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
@@ -12,14 +10,16 @@ export default function AboutMe() {
             <div className="about-section-img">
                 <img src={testImg} alt="about Section" />
             </div>
-            <div className="about-section-text">
-                <h1 className="about-section-title">About Me</h1>
-                <p className="about-section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+            <div className="about-section-box">
+                <div className="about-section-text">
+                    <h1 className="about-section-title">About Me</h1>
+                    <p className="about-section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
             </div>
         </div>
       </section>
