6316ccdd9ed084bbeef470611ee578930d2af1e8
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-08-27 20:48:11 -0400

    add title to app.jsx

--- portfolio-website-react-app/src/App.jsx
+++ portfolio-website-react-app/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
-import  StarsCanvas from './canvas/StarsCanvas.jsx';
+import StarsCanvas from './canvas/StarsCanvas.jsx';
 import AboutMe from "./sections/AboutMe.jsx";
 import Experience from "./sections/Experience.jsx";
 import Portfolio from "./sections/Portfolio.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
 import Contact from "./sections/Contact.jsx";
 
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = "Michael's Portfolio Website";
+  }, []);
 
   return (
     <>
