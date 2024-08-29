36e660e395b6749d2ea20e636a228b9beb3569e7
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-08-28 20:38:39 -0400

    add new logo for website

--- portfolio-website-react-app/src/sections/Navbar.jsx
+++ portfolio-website-react-app/src/sections/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-scroll";
-import logo from '../assets/logo.svg';
+import logo from '../assets/M-iconPortfolio.png';
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
